5885f2c051931191e6be0c810ad71eff0b5b87db
Rayko Bozhurin <rbozhurin@gmail.com>
2021-05-10 11:36:19 +0300

    Update

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+function toggle() {
+    const header = document.querySelector('header');
+    header.classList.toggle('active');
+}
