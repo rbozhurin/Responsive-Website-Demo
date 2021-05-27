58b5f50fff3a9df968132068370c6ef6df53f871
Rayko Bozhurin <rbozhurin@gmail.com>
2021-05-27 18:23:20 +0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,12 @@ function toggle() {
     const header = document.querySelector('header');
     header.classList.toggle('active');
 }
+
+function readMore() {
+    let button = document.querySelector('.btn');
+    let divExtra = document.querySelector('.extra');
+
+    divExtra.style.display = divExtra.style.display !== 'block' ? 'block' : 'none';
+    button.textContent = button.textContent === 'Read Our Story' ? 'Less' : 'Read Our Story';
+
+}
