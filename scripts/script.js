8ebd8d2fdeb6b7416b95441b71c171698ffb053d
Rayko Bozhurin <rbozhurin@gmail.com>
2021-05-31 20:31:12 +0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,21 @@ function toggle() {
     header.classList.toggle('active');
 }
 
+//About Us Section
 function readMore() {
     let button = document.querySelector('.btn');
     let divExtra = document.querySelector('.extra');
 
     divExtra.style.display = divExtra.style.display !== 'block' ? 'block' : 'none';
     button.textContent = button.textContent === 'Read Our Story' ? 'Less' : 'Read Our Story';
-
 }
+
+//After Sending a Message in Contact Form
+document.getElementById('sent')
+    .addEventListener('click', () => {
+        let contactForm = document.querySelector('.contactForm');
+        let thanksMessage = document.querySelector('.hidden');
+
+        contactForm.style.display = 'none';
+        thanksMessage.style.display = 'block';
+    })
